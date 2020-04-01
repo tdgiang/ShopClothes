eb4fdbaf8403da1dea620665f71600ce11d18ff2
Tran Duc Giang <tdgiangdev@gmail.com>
2020-04-01 16:40:16 +0700

    insert img

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { 
+    View
+ } from 'react-native';
+
+export default class app extends Component {
+   
+    render() {
+        return (
+            <View>
+                
+            </View>
+            
+        );
+    }
+}
